08dbc18084fce99c21858d1e12872657f4856b63
Georgi Santana <georgi.andersson@hotmail.com>
2023-07-31 11:20:15 -0300

    Criado projeto React-Native e inicializado exercicios

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {View, StyleSheet} from 'react-native';
+import X, {Comp1, Comp2} from './components/Multi';
+import Primeiro from './components/Primeiro';
+
+export default () => (
+  <View style={style.App}>
+    <X />
+    <Comp1 />
+    <Comp2 />
+    <Primeiro />
+  </View>
+);
+
+const style = StyleSheet.create({
+  App: {
+    flexGrow: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
+})
