5f44b3610062cef6564f5ac7e4917b4f3b1ad312
Georgi Santana <georgi.andersson@hotmail.com>
2023-08-14 00:49:07 -0300

    Trabalhando com fragment

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {View, StyleSheet} from 'react-native';
+import Titulo from './components/Titulo';
 //import X, {Comp1, Comp2} from './components/Multi';
 //import Primeiro from './components/Primeiro';
 import MinMax from './components/MinMax';
@@ -7,8 +8,12 @@ import Aleatorio from './components/Aleatorio';
 
 export default () => (
   <View style={style.App}>
-    <Aleatorio min={10} max={60}/>
-    {/*<MinMax min={3} max={20} />
+    <Titulo
+      principal="Cadastro Produto"
+      secundario="Tela de Cadastro do Produto"
+    />
+    {/*<Aleatorio min={10} max={60}/>
+    <MinMax min={3} max={20} />
     <MinMax min="1" max="94" />
     <X />
     <Comp1 />
